9b9abd46c998811eff5d444c288431cae4e53e04
Yoyo77400 <yohan.georgelin@gmail.com>
2025-01-21 17:41:42 +0100

    commit pour exercice votes factory

--- contracts/VotedNFT.sol
+++ contracts/VotedNFT.sol
@@ -6,17 +6,22 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract VotedNFT is ERC721 {
 
     mapping(address => bool) whiteList;
+    mapping(address => uint8) accountQuantity;
 
     error mintWlError(bool , bool);
+    error alreadyMint(uint, uint);
+
     constructor(string memory _name, string memory _tag) ERC721(_name, _tag){}
 
     function addWhiteList(address whiteListed) public {
         whiteList[whiteListed] = true ;
     }
 
-    function mintNFT() public {
+    function mintNFT(address _account) external payable {
         require(whiteList[msg.sender] == true, mintWlError(true, whiteList[msg.sender]));
+        require(accountQuantity[msg.sender] == 0, alreadyMint(0, accountQuantity[msg.sender]));
         
+        _safeMint(_account, 1);
     }
 
 }
